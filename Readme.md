d10698a8a7e9b53148d1bcf5e5c8bc82183bdcd4
Melike Nur <melikeenurky@gmail.com>
2025-02-28 12:25:38 +0300

    Start

---
+++ Readme.md
@@ -0,0 +1,13 @@
+### Navbar 
+```
+
+A. Ziyaretçi Baykartech sitesindeki bütün navbar elementlerine tıklayabilmeli ve sayfa sorunsuz açılmalı.
+a. Navbar otomasyonu dinamik yapıda olmalı, navbara yeni bir element eklendiğinde
+otomasyonu güncellemek zorunda kalmamalıyız. Kontrol döngü halinde tek metot içinde
+tıklama ve doğrulama işlemleri yapılmalıdır.
+
+1. Baykartech web sitesini aç. ✅
+2. Sayfayı boyutunu büyült. ✅
+3. Navbar içerisinde yer alan tüm elementlere tıkla. (tıkladığını kanıtla/dropdown dahil.)
+4.
+```
--- src/test/java/BaykartechNavbar.java
+++ src/test/java/BaykartechNavbar.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaykartechNavbar {
+    public static void main(String[] args) {
+
+        BaykartechStart();
+
+
+
+    }
+    public static void BaykartechStart(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://kariyer.baykartech.com/tr/");
+        driver.manage().window().maximize();
+        System.out.println("Sayfa açıldı ve büyütüldü");
+    }
+
 }
